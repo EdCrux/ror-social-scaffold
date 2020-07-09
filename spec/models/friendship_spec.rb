2408879f7256bde5b648cf285bdba2ce9c520cc8
Eduardo <jecruxiz@gmail.com>
2020-07-09 15:19:41 -0500

    :white_check_mark: Add and update test for user's friends methods

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,20 +1,10 @@
 
 require 'rails_helper'
-#class Friendship < ApplicationRecord
- # belongs_to :user
-  #belongs_to :friend, class_name: 'User'
-#end
 RSpec.describe Friendship , type: :model do
   describe Friendship do
     it do
       should belong_to(:user)
       should belong_to(:friend).class_name('User')
     end
-
-    it 'validations' do
-      should validate_presence_of(:friend_id)
-      should validate_presence_of(:user_id)
-    end
   end
-    
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let!(:user1){
+    User.create!(
+      email: 'fake@email.com',
+      password: 'password',
+      name: 'fake_test'
+    )
+  }
+
+  let!(:user2){
+    User.create!(
+      email: 'test@email.com',
+      password: 'test123',
+      name: 'friend_test'
+    )
+  }
+  let!(:friendship){
+    Friendship.create!(
+      user: user1,
+      friend: user2,
+    )
+  }
+  describe 'Actions for users' do
+    it '#confirm_friend' do
+      expect(user2.confirm_friend(user1)).to be(true)
+    end
+
+    it '#friend? -> false' do
+      expect(user2.friend?(user1)).to be(false)
+    end
+    
+    
+    it '#friend? -> true' do
+      user2.confirm_friend(user1)
+      expect(user1.friend?(user2)).to be(true)
+    end
+  end
+end
