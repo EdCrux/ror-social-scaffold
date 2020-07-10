1bf1834348968a795debc0a990558aed875f8280
Eduardo <jecruxiz@gmail.com>
2020-07-10 12:56:22 -0500

    :white_check_mark: Add test for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,20 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
+RSpec.describe Comment, type: :model do
   describe 'validations' do
     it 'validate content' do
       should validate_presence_of(:content)
       should validate_length_of(:content)
-        .is_at_most(1000)
-        .with_message('1000 characters in post is the maximum allowed.')
+        .is_at_most(200)
+        .with_message('200 characters in comment is the maximum allowed.')
     end
   end
 
   describe 'associations' do
     it do
       should belong_to(:user)
-      should have_many(:comments).dependent(:destroy)
-      should have_many(:likes).dependent(:destroy)
+      should belong_to(:post)
     end
   end
 
