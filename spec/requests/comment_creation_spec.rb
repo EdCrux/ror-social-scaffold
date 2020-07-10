151668a0c2e0a56b0d62be5c2ffea11e316b31ea
Eduardo <jecruxiz@gmail.com>
2020-07-10 15:41:49 -0500

    :white_check_mark: Add test for comment and post creation feature

---
+++ spec/requests/comment_creation_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'Post creation', type: :feature do
+  before :each do
+    User.create(name: 'Kitpao', email: 'kit@pao.com', password: 'password')
+    visit 'users/sign_in'
+    within('#new_user') do
+      fill_in 'user_email', with: 'kit@pao.com'
+      fill_in 'user_password', with: 'password'
+    end
+
+    click_button 'commit'
+    fill_in 'post_content', with: 'This is the content of the post'
+    click_button 'commit', value: 'Save'
+    fill_in 'comment_content', with: 'This is the comment'
+    click_button 'commit', value: 'Comment'
+    
+  end
+
+  it 'should have a content' do
+    expect(page).to have_content('This is the comment')
+  end
+end
