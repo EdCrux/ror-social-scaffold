7ca0e5a5543d87d6fe45118a10fbb34e2c2b45a0
Eduardo <jecruxiz@gmail.com>
2020-07-16 14:46:21 -0500

    :sparkles: Create confirm_friendships inside Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,13 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def self.confirm_friendship(user)
+    friendship_found = find_by(user_id: user.id)
+    friendship_found.confirmed = true
+    friendship_found.save
+    current_user = friendship_found.friend
+    new_friendship = current_user.friendships.build(friend: user, confirmed: true)
+    new_friendship.save
+  end
 end
