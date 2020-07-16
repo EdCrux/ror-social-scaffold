0bcf9ab62077bd680bf5fdebb51f74f1e10670cf
Eduardo <jecruxiz@gmail.com>
2020-07-16 16:38:27 -0500

    :white_check_mark: Update test for user model methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,7 +64,7 @@ RSpec.describe User, type: :model do
   end
 
   describe 'methods' do
-    let!(:user1) do
+    let!(:sender) do
       User.create!(
         email: 'fake@email.com',
         password: 'password',
@@ -72,7 +72,7 @@ RSpec.describe User, type: :model do
       )
     end
 
-    let!(:user2) do
+    let!(:receiver) do
       User.create!(
         email: 'test@email.com',
         password: 'test123',
@@ -81,47 +81,30 @@ RSpec.describe User, type: :model do
     end
     let!(:friendship) do
       Friendship.create!(
-        user: user1,
-        friend: user2
+        user: sender,
+        friend: receiver
       )
     end
     describe 'Actions for users' do
-      it '#friends -> empty array' do
-        expect(user1.friends).to be_empty
-      end
-
-      it '#friends -> not empty array' do
-        user2.confirm_friend(user1)
-        expect(user1.friends).to_not be_empty
-      end
-
-      it '#pending_friends -> not empty array' do
-        expect(user1.pending_friends).to_not be_empty
-      end
-
-      it '#pending_friends -> be empty' do
-        expect(user2.pending_friends).to be_empty
-      end
-
-      it '#friend_requests -> not empty' do
-        expect(user2.friend_requests).to_not be_empty
-      end
-      it '#friend_requests -> empty' do
-        user2.confirm_friend(user1)
-        expect(user2.friend_requests).to be_empty
-      end
-
-      it '#confirm_friend' do
-        expect(user2.confirm_friend(user1)).to be(true)
-      end
 
       it '#friend? -> false' do
-        expect(user2.friend?(user1)).to be(false)
+        expect(receiver.friend?(sender)).to be(false)
       end
 
-      it '#friend? -> true' do
-        user2.confirm_friend(user1)
-        expect(user1.friend?(user2)).to be(true)
+      it '#pending_friend? -> true' do
+        expect(sender.pending_friends?(receiver)).to be(true)
+      end
+
+      it '#pending_friend? -> false' do
+        expect(receiver.pending_friends?(sender)).to be(false)
+      end
+
+      it '#friend_requests? -> true' do
+        expect(receiver.friend_requests?(sender)).to be(true)
+      end
+
+      it '#friend_requests? -> false' do
+        expect(sender.friend_requests?(receiver)).to be(false)
       end
     end
   end
